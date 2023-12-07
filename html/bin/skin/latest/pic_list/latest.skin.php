347bef1ab32a638ecd104be629bc6c50f09b4132
hyebine <berryme015@gmail.com>
2023-12-07 11:00:57 +0900

    loginpage 수정

--- html/bin/skin/latest/pic_list/latest.skin.php
+++ html/bin/skin/latest/pic_list/latest.skin.php
@@ -1,22 +1,16 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 include_once(G5_LIB_PATH.'/thumbnail.lib.php');
-
-
 $thumb_width = 297;
 $thumb_height = 212;
 $list_count = (is_array($list) && $list) ? count($list) : 0;
-?>
-
+?>  
     <?php
     for ($i=0; $i<$list_count; $i++) {
         
-        $img_link_html = '';
-        
-        $wr_href = get_pretty_url($bo_table, $list[$i]['wr_id']);
-
-        if( $i === 0 ) {
-            $thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);
+        $img_link_html = '';        
+        $wr_href = get_pretty_url($bo_table, $list[$i]['wr_id']);     
+        $thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);
 
             if($thumb['src']) {
                 $img = $thumb['ori'];
@@ -24,22 +18,10 @@ $list_count = (is_array($list) && $list) ? count($list) : 0;
                 $img = G5_IMG_URL.'/no_img.png';
                 $thumb['alt'] = '이미지가 없습니다.';
             }
-            $img_content = '<img src="'.$img.'" alt="'.$list[$i]['subject'].'" >';
-            $img_link_html = '<a href="'.$wr_href.'" class="lt_img" >'.run_replace('thumb_image_tag', $img_content, $thumb).'</a>';
-        }
-    ?>
-       
-            
-            <?php
-           
-            echo "<a href=\"".$wr_href."\" class=\"pic_li_tit\"> ";
-            echo  $img_content;
-
-            echo "</a>";
-			
-
-            ?>
+          
+            $img_link_html = '<a href="http://'.$list[$i]['wr_link1'].'" class="lt_img" ><img src="'.$img.'" alt="'.$thumb['alt'].'" ></a>';
     
+    ?>       
+            <?php echo $img_link_html; ?>           
+        
     <?php }  ?>
-
-   
