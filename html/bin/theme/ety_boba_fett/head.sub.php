209b447bd1e8530e336939ed628b5df5ab68698f
hyebine <berryme015@gmail.com>
2023-12-07 11:59:03 +0900

    제이쿼리 바꾸기

--- html/bin/theme/ety_boba_fett/head.sub.php
+++ html/bin/theme/ety_boba_fett/head.sub.php
@@ -77,7 +77,7 @@ var g5_admin_url = "<?php echo G5_ADMIN_URL; ?>";
 <?php } ?>
 </script>
 <?php
-add_javascript('<script src="'.G5_JS_URL.'/jquery-1.12.4.min.js"></script>', 0);
+add_javascript('<script src="'.G5_JS_URL.'/jquery-2.2.4.min.js"></script>', 0);
 add_javascript('<script src="'.G5_JS_URL.'/jquery-migrate-1.4.1.min.js"></script>', 0);
 if (defined('_SHOP_')) {
     if(!G5_IS_MOBILE) {
